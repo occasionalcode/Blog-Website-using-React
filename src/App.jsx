80d9a3d0580ade9cde4aa9c41b9016322a330383
GABRIEL IBANEZ <occasionalprogrammer@gmail.com>
2024-04-10 17:46:46 +0800

    Blog website

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './create';
+import BlogDetails from './BlogDetails';
+import NotFound from './notFound';
+
+
+// install node 'npm i'
+// to start web 'npm run start'
+// to start server 'npx json-server --watch data/db.json --port 8000'
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
